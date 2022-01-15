20d471133832acf17cd6416b3cc153a3f4d12251
Knut Melvær <knut.melvaer@gmail.com>
2022-01-14 16:20:12 -0800

    add components

---
+++ components/checkMark.tsx
@@ -0,0 +1,17 @@
+export default function CheckMark() {
+  return (
+    <svg
+      width="28"
+      height="23"
+      viewBox="0 0 28 23"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 12.0488L10.5055 20.5543L26.6658 0.991699"
+        stroke="#F36458"
+        strokeWidth="3"
+      />
+    </svg>
+  );
+}
