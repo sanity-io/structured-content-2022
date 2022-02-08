13117f4f0b9472111b0a363aa0529b859cb5a6fa
Jonas Jensen <jonas.jensen@msn.com>
2022-02-08 12:47:10 +0100

    forwardRef in SectionBlock

--- web/components/SectionBlock/SectionBlock.tsx
+++ web/components/SectionBlock/SectionBlock.tsx
@@ -1,9 +1,17 @@
 import clsx from 'clsx';
-import { HTMLAttributes } from 'react';
+import { forwardRef, HTMLAttributes } from 'react';
 import styles from './SectionBlock.module.css';
 
 interface SectionBlockProps extends HTMLAttributes<HTMLDivElement> {}
 
-export const SectionBlock = ({ className, ...props }: SectionBlockProps) => (
-  <div className={clsx(className, styles.sectionBlock)} {...props} />
+export const SectionBlock = forwardRef<HTMLDivElement, SectionBlockProps>(
+  ({ className, ...props }: SectionBlockProps, ref) => (
+    <div
+      {...props}
+      className={clsx(className, styles.sectionBlock)}
+      ref={ref}
+    />
+  )
 );
+
+SectionBlock.displayName = 'SectionBlock';
