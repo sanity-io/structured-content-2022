b2b1feadb0e501f64968589d0a0cd1270c56db5c
Jonas Jensen <jonas.jensen@msn.com>
2022-02-14 16:10:51 +0100

    Add catch-all [[...slug]].tsx route
    This is a page which serves to render 'route'-type entities and their associated 'page' entities.

---
+++ web/pages/[[...slug]].tsx
@@ -0,0 +1,57 @@
+import { groq } from "next-sanity";
+import client from "../lib/sanity.server";
+import { Section } from "../types/Section";
+import SectionBlock from "../components/SectionBlock";
+import TextBlock from "../components/TextBlock";
+import Heading from "../components/Heading";
+import PageContainer from "../components/PageContainer";
+import Nav from "../components/Nav";
+
+const QUERY = groq`
+  {
+    "route": *[_type == "route" && slug.current == $slug][0] {
+      ...,
+      page-> {
+        name,
+        sections
+      }
+    }
+  }`;
+
+interface RouteProps {
+  data: {
+    route: {
+      page: {
+        name: string;
+        sections: Section[];
+      }
+    }
+  }
+}
+
+const Route = ({ data: { route: { page: { name, sections } } } }: RouteProps) => {
+  return (
+    <PageContainer>
+      <header>
+        <Nav/>
+        <SectionBlock>
+          <Heading>{name}</Heading>
+        </SectionBlock>
+      </header>
+      <SectionBlock>
+        <TextBlock value={sections}/>
+      </SectionBlock>
+    </PageContainer>
+  );
+};
+
+export async function getServerSideProps({ params }) {
+  const data = await client.fetch(QUERY, { slug: params?.slug?.[0] });
+  if (!data?.route?.page) {
+    return { notFound: true };
+  }
+
+  return { props: { data } };
+}
+
+export default Route;
