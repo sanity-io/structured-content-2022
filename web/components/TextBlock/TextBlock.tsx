e5374c099848d8d8c11070585a289e4a362f2d79
Jonas Jensen <jonas.jensen@msn.com>
2022-02-15 13:25:18 +0100

    Rewrite TextBlock component to correctly render Sanity content
    After the last couple of commits, the session I was using to test rendering, was no longer rendering its content correctly. This refactor makes the component make more sense, defining inline components for each entity mentioned by the example Session, so that the session once again renders correctly.

--- web/components/TextBlock/TextBlock.tsx
+++ web/components/TextBlock/TextBlock.tsx
@@ -5,50 +5,58 @@ import { RichTextSection } from '../../types/RichTextSection';
 import { getEntityPath } from "../../util/entityPaths";
 import Paragraph from '../Paragraph';
 
+const RichText = ({ value }) => (
+  <>
+    {value.content
+      .reduce((acc, content) => [...acc, ...content.children], [])
+      .map((children) => (
+        <Paragraph key={children._key}>{children.text}</Paragraph>
+      ))}
+  </>
+);
+
+const Person = ({ value: speaker }) => (
+  <Link href={getEntityPath(speaker)}>
+    <a>{speaker.name}</a>
+  </Link>
+)
+
+const Venue = ({ value: venue }) => (
+  <Link href={getEntityPath(venue)}>
+    <a>{venue.title}</a>
+  </Link>
+);
+
 const components: Partial<PortableTextComponents> = {
   types: {
-    richText: ({ value }) => (
-      <>
-        {value.content
-          .reduce((acc, content) => [...acc, ...content.children], [])
-          .map((children) => (
-            <Paragraph key={children._key}>{children.text}</Paragraph>
-          ))}
-      </>
-    ),
-    person: ({ value: speaker }) => {
-      return (
-        <Link href={getEntityPath(speaker)}>
-          <a>{speaker.name}</a>
-        </Link>
-      );
-    },
-    venue: ({ value: venue }) => (
-      <Link href={getEntityPath(venue)}>
-        <a>{venue.title}</a>
-      </Link>
-    ),
-  },
-  block: {
-    block: ({ value }) => (
-      <>
-        {value.children.map((children) => (
-          <Paragraph key={children._key}>{children.text}</Paragraph>
-        ))}
-      </>
-    ),
-  },
+    richText: RichText,
+    person: Person,
+    venue: Venue,
+    block: ({ value }) =>
+      value.children.map((child, index) => {
+          if (!child._key && child._type) {
+            switch (child._type) {
+              case 'richText':
+                return <RichText value={child}/>;
+              case 'person':
+                return <Person value={child}/>;
+              case 'venue':
+                return <Venue value={child}/>;
+              default:
+                return null;
+            }
+          }
 
+          return <span key={index}>{child.text}</span>;
+        }
+      ),
+  },
   marks: {
     bold: ({ children }) => <strong>{children}</strong>,
     italic: ({ children }) => <em>{children}</em>,
     underline: ({ children }) => <u>{children}</u>,
     code: ({ children }) => <code>{children}</code>,
-    internalLink: ({ text, value }) => (
-      <Link href={`/${value?.reference?._type}s/${value?.reference?._id}`}>
-        {text}
-      </Link>
-    ),
+    internalLink: ({ text, value }) => <Link href={getEntityPath(value.reference)}>{text}</Link>,
   },
 };
 
@@ -61,4 +69,4 @@ interface TextBlockProps {
 }
 
 export const TextBlock = ({ value }: TextBlockProps) =>
-  value ? <PortableText value={value} components={components} /> : <></>;
+  value ? <PortableText value={value} components={components}/> : <></>;
