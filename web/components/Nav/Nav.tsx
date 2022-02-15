155c5774927d66385713acccc7c2413386ee2dad
Øyvind Stenhaug <oyvind@minus.no>
2022-02-15 20:21:30 +0100

    Add basic mobile adaptation for the global Nav

--- web/components/Nav/Nav.tsx
+++ web/components/Nav/Nav.tsx
@@ -1,44 +1,76 @@
+import clsx from 'clsx';
 import Link from 'next/link';
+import { useState } from 'react';
 import ButtonLink from '../ButtonLink';
 import PageContainer from '../PageContainer';
 import logo from '../../images/logo.svg';
 import styles from './Nav.module.css';
 
-export const Nav = () => (
-  <nav className={styles.nav}>
-    <PageContainer>
-      <div className={styles.wrapper}>
-        <Link href="/home">
-          <a>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={logo.src}
-              className={styles.logo}
-              width={logo.width}
-              height={logo.height}
-              alt="Home"
-            />
-          </a>
-        </Link>
-        <ul className={styles.items}>
-          <li>
-            <Link href="/program">
-              <a className={styles.link}>Program</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/speakers">
-              <a className={styles.link}>Speakers</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a className={styles.link}>About</a>
-            </Link>
-          </li>
-        </ul>
-        <ButtonLink url="/tickets" text="Tickets" />
-      </div>
-    </PageContainer>
-  </nav>
-);
+export const Nav = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  function handleClose() {
+    setMenuOpened(false);
+  }
+  return (
+    <nav className={styles.nav}>
+      <PageContainer>
+        <div className={styles.menuButtonWrapper}>
+          <button
+            className={styles.menuButton}
+            type="button"
+            onClick={() => setMenuOpened(true)}
+          >
+            Menu
+          </button>
+        </div>
+        <div className={clsx(styles.menuContents, !menuOpened && styles.closed)}>
+          <Link href="/home">
+            <a className={styles.homeLink}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={logo.src}
+                className={styles.logo}
+                width={logo.width}
+                height={logo.height}
+                alt="Home"
+              />
+            </a>
+          </Link>
+          <ul className={styles.items}>
+            <li className={styles.ticketItem}>
+              <Link href="/tickets">
+                <a className={styles.link}>Tickets</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/program">
+                <a className={styles.link}>Program</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/speakers">
+                <a className={styles.link}>Speakers</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a className={styles.link}>About</a>
+              </Link>
+            </li>
+          </ul>
+          <div className={styles.ticketButton}>
+            <ButtonLink url="/tickets" text="Tickets" />
+          </div>
+          <button
+            type="button"
+            className={styles.closeButton}
+            onClick={() => setMenuOpened(false)}
+          >
+            Close
+          </button>
+        </div>
+      </PageContainer>
+    </nav>
+  );
+};
