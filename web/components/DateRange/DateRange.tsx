ef193cde04ee6018c4f07be5e816e52285e276bb
Jonas Jensen <jonas.jensen@msn.com>
2022-02-16 11:06:27 +0100

    DateRange component to display a range of two dates, formatted

---
+++ web/components/DateRange/DateRange.tsx
@@ -0,0 +1,19 @@
+import { formatDateRange } from "../../util/date";
+
+interface DateRangeProps {
+  startDate: string;
+  endDate: string;
+}
+
+export const DateRange = ({ startDate, endDate }: DateRangeProps) => {
+  if (!startDate || !endDate) {
+    return null;
+  }
+
+  const { start, end } = formatDateRange(startDate, endDate);
+  return (
+    <>
+      <time dateTime={start}>{start.trim()}</time>–<time dateTime={end}>{end.trim()}</time>
+    </>
+  )
+}
--- web/util/date.ts
+++ web/util/date.ts
@@ -1,4 +1,30 @@
 import { format } from 'date-fns';
 
 export const formatDateWithTime = (date: string) =>
-  format(new Date(date), 'MMMM d, yyyy hh:mm');
+  format(new Date(date), 'MMMM d, yyyy');
+
+const monthNames = [
+  "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+
+const utcDate = (date: string) => {
+  const d = new Date(date);
+  return {
+    day: d.getUTCDate(),
+    month: d.getUTCMonth(),
+    year: d.getUTCFullYear(),
+  }
+}
+
+export const formatDateRange = (date1: string, date2: string) => {
+  const d1 = utcDate(date1);
+  const d2 = utcDate(date2);
+
+  const y1 = Math.abs(d2.year - d1.year) > 0 ? `, ${d1.year}` : ''
+  const m2 = Math.abs(d2.month - d1.month) > 0 ? ` ${monthNames[d2.month]} ` : ''
+
+  return {
+    start: `${monthNames[d1.month]} ${d1.day}${y1}`,
+    end: `${m2}${d2.day}, ${d2.year}`,
+  }
+};
