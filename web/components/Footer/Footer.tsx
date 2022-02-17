10c1b0531bbf8512b649b3a5f00f9b73685f3c83
Øyvind Stenhaug <oyvind@minus.no>
2022-02-17 18:54:31 +0100

    Update markup and CSS for footer
    - Use GridWrapper for footer as well, for consistent horizontal
      width/padding
    - Use a list for the social links, seems semantically better than divs
      (the links after the separator, to CoC etc, already use a <ul>)
    - Adjust various spacings and font according to Figma sketch for mobile
    
    (styling for wider screens coming next)

--- web/components/Footer/Footer.tsx
+++ web/components/Footer/Footer.tsx
@@ -4,71 +4,85 @@ import sanityLogo from '../../images/sanity_logo_black.svg';
 import instagramLogo from '../../images/instagram_logo_black.svg';
 import twitterLogo from '../../images/twitter_logo_black.svg';
 import linkedinLogo from '../../images/linkedin_logo_black.svg';
+import GridWrapper from '../GridWrapper';
 import Paragraph from '../Paragraph';
 import styles from './Footer.module.css';
 
 export const Footer = () => (
   <footer className={styles.container}>
-    <div className={styles.mainContent}>
+    <GridWrapper>
       <div className={styles.logoContainer}>
         <Image src={sanityLogo} alt="Sanity" width={139} height={28} />
       </div>
-      <Paragraph>
-        Structured Content 2022 is a conference by Sanity
-      </Paragraph>
+
+      <Paragraph>Structured Content 2022 is a conference by Sanity</Paragraph>
 
       <Paragraph>
         Inquiries:
-        <a
-          className={styles.mailLink}
-          href="mailto:email@sanity.io"
-        >
+        <a className={styles.mailLink} href="mailto:email@sanity.io">
           email@sanity.io
         </a>
       </Paragraph>
-    </div>
 
-    <div className={styles.social}>
-      <div className={styles.socialItem}>
-        <a href="https://www.instagram.com/sanity.io" target="_blank" rel="noreferrer">
-          <Image src={instagramLogo} alt="Instagram" width={24} height={24} />
-        </a>
-      </div>
-      <div className={styles.socialItem}>
-        <a href="https://twitter.com/sanity_io" target="_blank" rel="noreferrer">
-          <Image src={twitterLogo} alt="Twitter" width={24} height={24} />
-        </a>
-      </div>
-      <div className={styles.socialItem}>
-        <a href="https://www.linkedin.com/company/sanity-io" target="_blank" rel="noreferrer">
-        <Image src={linkedinLogo} alt="LinkedIn" width={24} height={24} />
-        </a>
-      </div>
-    </div>
+      <ul className={styles.social}>
+        <li className={styles.socialItem}>
+          <a
+            href="https://www.instagram.com/sanity.io"
+            className={styles.socialLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image src={instagramLogo} alt="Instagram" width={24} height={24} />
+          </a>
+        </li>
+        <li className={styles.socialItem}>
+          <a
+            href="https://twitter.com/sanity_io"
+            className={styles.socialLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image src={twitterLogo} alt="Twitter" width={24} height={24} />
+          </a>
+        </li>
+        <li className={styles.socialItem}>
+          <a
+            href="https://www.linkedin.com/company/sanity-io"
+            className={styles.socialLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image src={linkedinLogo} alt="LinkedIn" width={24} height={24} />
+          </a>
+        </li>
+      </ul>
+    </GridWrapper>
 
     <hr className={styles.separator} />
 
-    <ul className={styles.links}>
-      <li className={styles.linksItem}>
-        <Link href="#">
-          <a className={styles.linksItemLink}>Code of conduct</a>
-        </Link>
-      </li>
-      <li className={styles.linksItem}>
-        <Link href="#">
-          <a className={styles.linksItemLink}>Privacy policy</a>
-        </Link>
-      </li>
-      <li className={styles.linksItem}>
-        <Link href="#">
-          <a className={styles.linksItemLink}>Contact</a>
-        </Link>
-      </li>
-      <li className={styles.linksItem}>
-        <Link href="#">
-          <a className={styles.linksItemLink}>Venues</a>
-        </Link>
-      </li>
-    </ul>
+    <GridWrapper>
+      <ul className={styles.links}>
+        <li className={styles.linksItem}>
+          <Link href="#">
+            <a className={styles.linksItemLink}>Code of conduct</a>
+          </Link>
+        </li>
+        <li className={styles.linksItem}>
+          <Link href="#">
+            <a className={styles.linksItemLink}>Privacy policy</a>
+          </Link>
+        </li>
+        <li className={styles.linksItem}>
+          <Link href="#">
+            <a className={styles.linksItemLink}>Contact</a>
+          </Link>
+        </li>
+        <li className={styles.linksItem}>
+          <Link href="#">
+            <a className={styles.linksItemLink}>Venues</a>
+          </Link>
+        </li>
+      </ul>
+    </GridWrapper>
   </footer>
 );
