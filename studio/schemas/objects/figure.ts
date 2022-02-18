8f1cb6deb0fe30812b75851fcaf659146891445e
Knut Melvær <knut@sanity.io>
2022-02-17 19:32:47 -0800

    add icon

--- studio/schemas/objects/figure.ts
+++ studio/schemas/objects/figure.ts
@@ -1,7 +1,9 @@
+import { ImageIcon } from "@sanity/icons";
 export default {
   name: "figure",
   title: "Figure",
   type: "image",
+  icon: ImageIcon,
   description: "An image with alternative text and an optional caption",
   options: {
     hotspot: true,
