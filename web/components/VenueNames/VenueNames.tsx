d9e6a65931816ee7520e3eea7bddc6a77f0ceb7e
Øyvind Stenhaug <oyvind@minus.no>
2022-02-21 12:10:16 +0100

    Add link markup to VenueNames
    If I understand correctly, these will be clickable after all. Real URLs
    to be added in a future commit, these are just placeholders for styling
    purposes.

--- web/components/VenueNames/VenueNames.tsx
+++ web/components/VenueNames/VenueNames.tsx
@@ -1,14 +1,18 @@
-import { Venue } from "../../types/Venue";
-import styles from "./VenueNames.module.css";
+import { Venue } from '../../types/Venue';
+import styles from './VenueNames.module.css';
 
 interface VenueNamesProps {
   venues: Venue[];
 }
 
-export const VenueNames = ({ venues }: VenueNamesProps) =>
+export const VenueNames = ({ venues }: VenueNamesProps) => (
   <ul className={styles.venues}>
-    {venues.map(venue =>
+    {venues.map((venue) => (
       <li key={venue.title} className={styles.venue}>
-        {venue.title}
-      </li>)}
+        <a href="#" className={styles.link}>
+          {venue.title}
+        </a>
+      </li>
+    ))}
   </ul>
+);
