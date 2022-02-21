83fc46acd1926c89380c0a2d2766179e2deccb5e
Øyvind Stenhaug <oyvind@minus.no>
2022-02-21 19:49:14 +0100

    Add frontend adjustments for ConferenceUpdatesForm
    Based on Figma sketches, but with some shortcuts and simplifications due
    to time constraints.

--- web/components/ConferenceUpdatesForm/ConferenceUpdatesForm.tsx
+++ web/components/ConferenceUpdatesForm/ConferenceUpdatesForm.tsx
@@ -1,37 +1,46 @@
+import GridWrapper from '../GridWrapper';
+import Heading from '../Heading';
 import Paragraph from '../Paragraph';
 import PlaceholderImage from '../PlaceholderImage';
-import Heading from '../Heading';
 import styles from './ConferenceUpdatesForm.module.css';
 
 export const ConferenceUpdatesForm = () => (
-  <div className={styles.container}>
-    <PlaceholderImage width={330} height={459} />
+  <GridWrapper>
+    <article className={styles.container}>
+      <PlaceholderImage width={330} height={459} className={styles.image} />
 
-    <div>
-      <Heading type="h2">Get conference updates</Heading>
-      <form
-        action="https://sanity.us3.list-manage.com/subscribe/post"
-        method="POST"
-      >
-        <input type="hidden" name="u" value="3e99a07b5e03ed5b07a234a57" />
-        <input type="hidden" name="id" value="cca563332b" />
-        <div>
-          <input
-            id="mce-EMAIL"
-            name="EMAIL"
-            type="email"
-            placeholder="Email address"
-            required
-            className={styles.emailInput}
-          />
-          <button type="submit" className={styles.submitButton}>
-            Sign up
-          </button>
-        </div>
-      </form>
-      <Paragraph className={styles.emailParagraph}>
-        We&#39;ll only send you updates about the conference
-      </Paragraph>
-    </div>
-  </div>
+      <div className={styles.mainContents}>
+        <Heading type="h2">Get conference updates</Heading>
+        <form
+          action="https://sanity.us3.list-manage.com/subscribe/post"
+          method="POST"
+          className={styles.form}
+        >
+          <div className={styles.formContents}>
+            <input type="hidden" name="u" value="3e99a07b5e03ed5b07a234a57" />
+            <input type="hidden" name="id" value="cca563332b" />
+            <input
+              id="mce-EMAIL"
+              name="EMAIL"
+              type="email"
+              placeholder="Your email address"
+              aria-label="Your email address"
+              required
+              className={styles.emailInput}
+            />
+            <button
+              type="submit"
+              className={styles.submitButton}
+              aria-label="Sign up"
+            >
+              -&gt;
+            </button>
+          </div>
+        </form>
+        <p className={styles.emailParagraph}>
+          We&#39;ll only send you updates about the conference
+        </p>
+      </div>
+    </article>
+  </GridWrapper>
 );
