cecff5b05f0ea2f1467e1f916f1a5d2081dfe16c
Knut Melvær <knut@sanity.io>
2022-02-21 14:10:33 -0800

    add new venues section

---
+++ studio/schemas/sections/venues.ts
@@ -0,0 +1,42 @@
+export default {
+  name: "venues",
+  type: "object",
+  title: "Venues section",
+  fields: [
+    {
+      name: "type",
+      type: "string",
+      title: "Section type",
+      description: "",
+      options: {
+        list: [
+          { title: "All venues", value: "all" },
+          { title: "Highlighted venues", value: "highlighted" },
+          { title: "No venues", value: "none" },
+        ],
+      },
+    },
+    {
+      name: "venues",
+      type: "array",
+      title: "Venues",
+      hidden: ({ parent }) => parent?.type !== "highlighted",
+      of: [
+        {
+          type: "reference",
+          to: [{ type: "venue" }],
+          options: {
+            // Just include venues that's part of an event, and that hasn't been selected already
+            filter: ({ parent }) => ({
+              filter:
+                '_id in *[_type == "event"].venues[]._ref && !(_id in $current)',
+              params: {
+                current: parent?.map(({ _ref }) => _ref),
+              },
+            }),
+          },
+        },
+      ],
+    },
+  ],
+};
