b0fdd14d81810e824f384ae15aff738ee4e432cb
Jonas Jensen <jonas.jensen@msn.com>
2022-02-21 12:29:49 +0100

    VenueNames.tsx: Link to each venue

--- web/components/VenueNames/VenueNames.tsx
+++ web/components/VenueNames/VenueNames.tsx
@@ -1,4 +1,5 @@
 import { Venue } from '../../types/Venue';
+import { getEntityPath } from "../../util/entityPaths";
 import styles from './VenueNames.module.css';
 
 interface VenueNamesProps {
@@ -9,7 +10,7 @@ export const VenueNames = ({ venues }: VenueNamesProps) => (
   <ul className={styles.venues}>
     {venues.map((venue) => (
       <li key={venue.title} className={styles.venue}>
-        <a href="#" className={styles.link}>
+        <a href={getEntityPath(venue)} className={styles.link}>
           {venue.title}
         </a>
       </li>
