dc69d2deb62ac1e1e57a3aa57224c946165e21cd
Øyvind Stenhaug <oyvind@minus.no>
2022-02-22 15:55:36 +0100

    Add CTA support to Hero component
    Not sure this will remain or not, but for now there is a link on the
    Program page, so might as well hook it up to a little bit of CSS

--- web/components/Hero/Hero.tsx
+++ web/components/Hero/Hero.tsx
@@ -1,15 +1,25 @@
+import ButtonLink from '../ButtonLink';
 import GridWrapper from '../GridWrapper';
 import styles from './Hero.module.css';
 
 interface HeroProps {
   heading: string;
+  cta?: {
+    url: string;
+    text: string;
+  };
 }
 
-export const Hero = ({ heading }: HeroProps) => (
+export const Hero = ({ heading, cta }: HeroProps) => (
   <div className={styles.container}>
     <GridWrapper>
       <div className={styles.headingContainer}>
         <h1 className={styles.heading}>{heading}</h1>
+        {cta && (
+          <div className={styles.cta}>
+            <ButtonLink text={cta.text} url={cta.url} />
+          </div>
+        )}
       </div>
     </GridWrapper>
   </div>
