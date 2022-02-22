c72fa4ba65969f1c3029c3a61288b91ba7c3b843
Øyvind Stenhaug <oyvind@minus.no>
2022-02-22 21:20:36 +0100

    WIP

--- web/components/TextBlock/TextAndImage.tsx
+++ web/components/TextBlock/TextAndImage.tsx
@@ -1,4 +1,5 @@
 import { imageUrlFor } from '../../lib/sanity';
+import GridWrapper from '../GridWrapper';
 import Heading from '../Heading';
 import { Block } from './Block';
 import styles from './TextAndImage.module.css';
@@ -11,17 +12,19 @@ interface TextAndImageProps {
 }
 
 export const TextAndImage = ({ value: { image, tagline, text, title } }) => (
-  <div className={styles.container}>
-    <div className={styles.image}>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={imageUrlFor(image).ignoreImageParams().url()} alt="" />
-    </div>
-    <div>
-      <Heading type="h2">{title}</Heading>
-      <Heading type="h3">{tagline}</Heading>
-      {text.map((value) => (
-        <Block key={value._key} value={value} />
-      ))}
-    </div>
-  </div>
+  <section className={styles.container}>
+    <GridWrapper>
+      <div className={styles.contents}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={imageUrlFor(image).ignoreImageParams().url()} alt="" className={styles.image} />
+        <div>
+          <Heading type="h2">{title}</Heading>
+          <Heading type="h3">{tagline}</Heading>
+          {text.map((value) => (
+            <Block key={value._key} value={value} />
+          ))}
+        </div>
+      </div>
+    </GridWrapper>
+  </section>
 );
