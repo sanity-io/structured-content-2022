6c8ffc6efa425943f33a09f3b8e3a5a098e04222
Øyvind Stenhaug <oyvind@minus.no>
2022-02-22 19:48:48 +0100

    Show RichText without yellow background by default
    Currently we don't have a way of toggling it on or a logic for when to
    do so (Sanity doesn't want to make this editor-controllable). Might do
    so later based on section index or similar.

--- web/components/TextBlock/RichText.tsx
+++ web/components/TextBlock/RichText.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { TextBlock } from './TextBlock';
 import GridWrapper from '../GridWrapper';
 import Heading from '../Heading';
@@ -6,11 +7,12 @@ import styles from './RichText.module.css';
 
 interface RichTextProps {
   value: RichTextSection;
+  background?: boolean;
 }
 
-export const RichText = ({ value }: RichTextProps) => (
+export const RichText = ({ value, background }: RichTextProps) => (
   <GridWrapper>
-    <div className={styles.container}>
+    <div className={clsx(styles.container, background && styles.background)}>
       <div className={styles.content}>
         {value.heading && <Heading type="h2">{value.heading}</Heading>}
         {value.subheading && <Heading type="h3">{value.subheading}</Heading>}
