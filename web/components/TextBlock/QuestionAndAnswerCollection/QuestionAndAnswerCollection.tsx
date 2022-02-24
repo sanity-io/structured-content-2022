2e14395bed24dd72e6b0f7e70ca99eed26f35d49
Øyvind Stenhaug <oyvind@minus.no>
2022-02-24 16:54:14 +0100

    Adjust markup and styling for QuestionAndAnswerCollection

--- web/components/TextBlock/QuestionAndAnswerCollection/QuestionAndAnswerCollection.tsx
+++ web/components/TextBlock/QuestionAndAnswerCollection/QuestionAndAnswerCollection.tsx
@@ -1,19 +1,21 @@
 import Heading from '../../Heading';
 import Block from '../Block';
-import GridWrapper from "../../GridWrapper";
+import GridWrapper from '../../GridWrapper';
 import styles from './QuestionAndAnswerCollection.module.css';
 
-export const QuestionAndAnswerCollection = ({
-  value: { questions },
-}) => (
+export const QuestionAndAnswerCollection = ({ value: { questions } }) => (
   <GridWrapper>
-    {questions.map(({ _key, question, answer }) => (
-      <div key={_key} className={styles.question}>
-        <Heading type="h3">{question}</Heading>
-        {answer.map((value) => (
-          <Block key={value._key} value={value} />
-        ))}
-      </div>
-    ))}
+    <div className={styles.container}>
+      {questions.map(({ _key, question, answer }) => (
+        <section key={_key} className={styles.section}>
+          <Heading type="h3">{question}</Heading>
+          <div className={styles.answer}>
+            {answer.map((value) => (
+              <Block key={value._key} value={value} />
+            ))}
+          </div>
+        </section>
+      ))}
+    </div>
   </GridWrapper>
 );
