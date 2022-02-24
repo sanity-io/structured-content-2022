2d9ae06a1548b878fab8c4055a53554a24a715d1
Jonas Jensen <jonas.jensen@msn.com>
2022-02-24 17:47:28 +0100

    Tickets.tsx: run prettier on files

--- web/components/TextBlock/Tickets/Tickets.tsx
+++ web/components/TextBlock/Tickets/Tickets.tsx
@@ -26,12 +26,60 @@ export const Tickets = ({ value: { type, tickets } }: TicketsProps) => {
   );
   return (
     <>
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <td />
-          {tickets.map((ticket) => (
-            <td key={ticket._id} className={clsx(styles.ticketInfo, styles.ticketInfoColumn)}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td />
+            {tickets.map((ticket) => (
+              <td
+                key={ticket._id}
+                className={clsx(styles.ticketInfo, styles.ticketInfoColumn)}
+              >
+                <div className={styles.name}>{ticket.type}</div>
+                <div>
+                  <div>Price</div>
+                  <div className={styles.price}>
+                    {ticket.price ? `$${ticket.price}` : 'Free'}
+                  </div>
+                </div>
+              </td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {includedTypes.map((includedType) => (
+            <tr key={includedType}>
+              <td className={styles.feature}>{includedType}</td>
+              {tickets.map((ticket) => {
+                const featureIncluded = ticket.included.includes(includedType);
+                return (
+                  <td
+                    key={ticket._id}
+                    className={clsx(
+                      styles.feature,
+                      featureIncluded && styles.featureIncluded
+                    )}
+                  >
+                    {featureIncluded ? (
+                      <div className={styles.checkmark}>&#10004;</div>
+                    ) : (
+                      <div className={styles.cross}>&#10008;</div>
+                    )}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className={styles.list}>
+        {tickets.map((ticket) => (
+          <>
+            <div
+              key={ticket._id}
+              className={clsx(styles.ticketInfo, styles.ticketInfoListItem)}
+            >
               <div className={styles.name}>{ticket.type}</div>
               <div>
                 <div>Price</div>
@@ -39,58 +87,20 @@ export const Tickets = ({ value: { type, tickets } }: TicketsProps) => {
                   {ticket.price ? `$${ticket.price}` : 'Free'}
                 </div>
               </div>
-            </td>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {includedTypes.map((includedType) => (
-          <tr key={includedType}>
-            <td className={styles.feature}>{includedType}</td>
-            {tickets.map((ticket) => {
-              const featureIncluded = ticket.included.includes(includedType);
-              return (
-                <td
-                  key={ticket._id}
-                  className={clsx(
-                    styles.feature,
-                    featureIncluded && styles.featureIncluded
-                  )}
-                >
-                  {featureIncluded ? (
-                    <div className={styles.checkmark}>&#10004;</div>
-                  ) : (
-                    <div className={styles.cross}>&#10008;</div>
-                  )}
-                </td>
-              );
-            })}
-          </tr>
+            </div>
+            <ul className={styles.ticketFeaturesListItem}>
+              {ticket.included.map((included) => (
+                <li key={included} className={styles.includedFeature}>
+                  <span className={styles.checkmark}>&#10004;</span>
+                  <span className={styles.includedFeatureDescription}>
+                    {included}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </>
         ))}
-      </tbody>
-    </table>
-
-    <div className={styles.list}>
-      {tickets.map((ticket) => (
-        <>
-        <div key={ticket._id} className={clsx(styles.ticketInfo, styles.ticketInfoListItem)}>
-          <div className={styles.name}>{ticket.type}</div>
-          <div>
-            <div>Price</div>
-            <div className={styles.price}>{ticket.price ? `$${ticket.price}` : 'Free'}</div>
-          </div>
-        </div>
-          <ul className={styles.ticketFeaturesListItem}>
-            {ticket.included.map((included) => (
-              <li key={included} className={styles.includedFeature}>
-                <span className={styles.checkmark}>&#10004;</span>
-                <span className={styles.includedFeatureDescription}>{included}</span>
-              </li>
-            ))}
-          </ul>
-        </>
-      ))}
-    </div>
-  </>
+      </div>
+    </>
   );
 };
