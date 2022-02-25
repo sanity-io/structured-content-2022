a52c12d8a96a07c55497cb574ef473fd306d069c
Knut Melvær <knut@sanity.io>
2022-02-24 16:36:03 -0800

    update ticketing with prices and availability

--- studio/schemas/documents/ticket.ts
+++ studio/schemas/documents/ticket.ts
@@ -1,4 +1,6 @@
+import { toPlainText } from "@portabletext/react";
 import { BillIcon } from "@sanity/icons";
+import { compareAsc, parseISO } from "date-fns";
 
 export default {
   name: "ticket",
@@ -8,12 +10,26 @@ export default {
   preview: {
     select: {
       title: "type",
-      subtitle: "price",
+      priceAndAvailability: "priceAndAvailability",
     },
-    prepare({ title, subtitle }) {
+    prepare({ title, priceAndAvailability }) {
+      const findCurrentPrice = priceAndAvailability?.reduce((acc, current) => {
+        const fromDate = parseISO(current.from);
+        const currentDate = new Date();
+        // Check if current date is before the from date
+        if (compareAsc(currentDate, fromDate) == 1) {
+          return current;
+        }
+        return acc;
+      }, priceAndAvailability[0]);
       return {
         title,
-        subtitle: subtitle ? `$${subtitle}` : "Price is missing",
+        subtitle: [
+          `Current price: $${findCurrentPrice?.price}`,
+          findCurrentPrice?.label,
+        ]
+          .filter(Boolean)
+          .join(" - "),
       };
     },
   },
@@ -24,33 +40,88 @@ export default {
       type: "string",
     },
     {
-      name: "price",
-      title: "Price",
-      type: "number",
-      description: "Price in USD",
-    },
-    {
-      name: "available",
-      title: "Available from-to",
-      type: "object",
-      fields: [
+      name: "priceAndAvailability",
+      title: "Price and availability",
+      type: "array",
+      description:
+        "Add a price and availability. If you don't add a price, the ticket will be free.",
+      of: [
         {
-          name: "from",
-          title: "Available form",
-          type: "datetime",
-          description: "This ticket is available to purchase from this date",
-        },
-        {
-          name: "to",
-          title: "Available to",
-          type: "datetime",
-          description: "This ticket is available to purchase until this date",
+          name: "available",
+          title: "Available from-to",
+          type: "object",
+          description:
+            "If no other date slot comes after, then the event date will be used as the final ticket date",
+          preview: {
+            select: {
+              price: "price",
+              from: "from",
+              label: "label",
+            },
+            prepare({ price, from, label }) {
+              return {
+                title: "$" + price,
+                subtitle: [new Date(from).toLocaleDateString(), label]
+                  .filter(Boolean)
+                  .join(" - "),
+              };
+            },
+          },
+          fields: [
+            {
+              name: "price",
+              title: "Price",
+              type: "number",
+              description: "Price in USD",
+            },
+            {
+              name: "from",
+              title: "Available from",
+              type: "datetime",
+              validation: (Rule) => [
+                Rule.required(),
+                Rule.custom((value, context) => {
+                  // find the index with this current value
+                  const { priceAndAvailability } = context?.document;
+                  const currentDate = parseISO(value);
+                  const index = priceAndAvailability.findIndex(
+                    ({ from }) => from === value
+                  );
+                  const previousDate = parseISO(
+                    priceAndAvailability[index - 1]?.from
+                  );
+                  if (!previousDate) {
+                    return true;
+                  }
+                  if (compareAsc(currentDate, previousDate) === -1) {
+                    return "Date must be after previous date";
+                  }
+
+                  return true;
+                }),
+              ],
+              description:
+                "This ticket is available to purchase from this date",
+            },
+            {
+              name: "label",
+              type: "string",
+              title: "Ticket price label (optional)",
+              description: 'For example: "Early bird"',
+            },
+          ],
         },
       ],
     },
     {
+      name: "description",
+      title: "Description",
+      type: "simpleBlockContent",
+    },
+    {
       name: "included",
       type: "array",
+      validation: (Rule) => [Rule.unique()],
       of: [
         {
           type: "string",
@@ -59,13 +130,26 @@ export default {
       options: {
         list: [
           {
-            title: "Workshop",
-            value: "workshop",
+            title: "Conference recordings after the event",
+            value: "Conference recordings after the event",
           },
           {
-            title: "Recordings",
-            value: "recordings",
+            title: "Access to the conference community",
+            value: "Access to the conference community",
           },
+          { title: "Live-streamed session", value: "Live-streamed session" },
+          { title: "Virtual networking", value: "Virtual networking" },
+          { title: "Swag kit", value: "Swag kit" },
+          {
+            title: "Access to in-person event",
+            value: "Access to in-person event",
+          },
+          {
+            title: "Access to in-person networking events",
+            value: "Access to in-person networking events",
+          },
+          { title: "Workshop", value: "Workshop" },
+          { title: "Recordings", value: "Recordings" },
         ],
       },
     },
