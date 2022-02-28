d67178c980c2aca83ed0326f99c389d6b5c26718
Øyvind Stenhaug <oyvind@minus.no>
2022-02-28 11:09:07 +0100

    Support url prop in SimpleCallToAction component
    This exists in the corresponding Studio code, supporting external URLs

--- web/components/TextBlock/SimpleCallToAction/SimpleCallToAction.tsx
+++ web/components/TextBlock/SimpleCallToAction/SimpleCallToAction.tsx
@@ -3,8 +3,10 @@ import { SimpleCallToAction as SimpleCallToActionProps } from '../../../types/Si
 import ButtonLink from '../../ButtonLink';
 
 export const SimpleCallToAction = ({
-  value: { text, reference },
+  value: { text, url, reference },
 }: PortableTextComponentProps<SimpleCallToActionProps>) =>
   text && reference?.slug?.current ? (
     <ButtonLink text={text} url={`/${reference?.slug?.current}`} />
+  ) : text && url ? (
+    <ButtonLink text={text} url={url} />
   ) : null;
--- web/types/SimpleCallToAction.ts
+++ web/types/SimpleCallToAction.ts
@@ -2,6 +2,7 @@ import { Slug } from './Slug';
 
 export type SimpleCallToAction = {
   text?: string;
+  url?: string;
   reference?: {
     slug?: Slug;
   };
