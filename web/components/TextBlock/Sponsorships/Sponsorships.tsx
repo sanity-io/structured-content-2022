eba753b8c6e3c4846b68d471734217a4737ff9ad
Jonas Jensen <jonas.jensen@msn.com>
2022-02-28 15:28:53 +0100

    Sponsorships.tsx: WIP support for all sponsorshipSection types
    Studio currently seems to reference to 'people' when attempting to add highlighted sponsors.

--- web/components/TextBlock/Sponsorships/Sponsorships.tsx
+++ web/components/TextBlock/Sponsorships/Sponsorships.tsx
@@ -3,22 +3,31 @@ import GridWrapper from "../../GridWrapper";
 
 interface SponsorshipsProps {
   value: {
-    type: string;
+    type: 'all' | 'highlighted' | 'none';
     sponsorships: Sponsorship[];
   }
 }
 
 export const Sponsorships = ({ value: { type, sponsorships } }: SponsorshipsProps) => {
-  if (type !== 'all') {
-    console.error(`Unrecognized Sponsorships type: '${type}'`);
+  if (type === 'none') {
     return null;
   }
 
-  return (
-    <GridWrapper>
-      {sponsorships.map(sponsorship => {
-        return null; // TODO: render content here
-      })}
-    </GridWrapper>
-  );
+  if (type === 'highlighted') {
+    // TODO: Sanity support required
+    return null;
+  }
+
+  if (type === 'all') {
+    return (
+      <GridWrapper>
+        {sponsorships.map(sponsorship => {
+          return null; // TODO: render content here
+        })}
+      </GridWrapper>
+    );
+  }
+
+  console.error(`Unrecognized Sponsorships type: '${type}'`);
+  return null;
 }
