ece38870f769589d52e51df39c5297ec91651205
Øyvind Stenhaug <oyvind@minus.no>
2022-02-28 18:02:27 +0100

    Copy/paste RichText wrappers/style to editorial article
    Workaround for not being able to use RichText directly here

--- web/pages/article/[slug].tsx
+++ web/pages/article/[slug].tsx
@@ -2,13 +2,15 @@ import clsx from 'clsx';
 import { groq } from 'next-sanity';
 import { useEffect, useState } from 'react';
 import Hero from '../../components/Hero';
-import RichText from '../../components/TextBlock/RichText';
+import TextBlock from '../../components/TextBlock';
 import Footer from '../../components/Footer';
 import Nav from '../../components/Nav';
 import client from '../../lib/sanity.server';
 import { Slug } from '../../types/Slug';
 import styles from '../app.module.css';
+import articleStyles from './article.module.css';
 import { mainEventId } from '../../util/entityPaths';
+import GridWrapper from '../../components/GridWrapper';
 import { Article } from '../../types/Article';
 
 const QUERY = groq`
@@ -61,7 +63,17 @@ const ArticleRoute = ({
     </header>
     <main>
       <Hero heading={heading} summary={summary} />
-      <RichText value={{ content }} />
+      {/* This is a workaround for TextBlock and the types it handles currently
+       * being tied to PortableText. Ideally we would just reuse the RichText
+       * component here instead of copy/pasting the wrappers/CSS.
+       */}
+      <GridWrapper>
+        <div className={articleStyles.container}>
+          <div className={articleStyles.content}>
+            <TextBlock value={content} />
+          </div>
+        </div>
+      </GridWrapper>
     </main>
     <Footer links={footer.links} />
   </>
