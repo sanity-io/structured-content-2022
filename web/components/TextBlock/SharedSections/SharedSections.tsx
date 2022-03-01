3077d0d9320b5faed372535d0b54cfc69092ae6a
Jonas Jensen <jonas.jensen@msn.com>
2022-03-01 12:04:41 +0100

    SharedSections.tsx: use existing Figure component, delete inlined Figure

--- web/components/TextBlock/SharedSections/SharedSections.tsx
+++ web/components/TextBlock/SharedSections/SharedSections.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image';
-import { imageUrlFor } from '../../../lib/sanity';
 import ConferenceUpdatesForm from '../../ConferenceUpdatesForm';
 import Heading from '../../Heading';
+import Figure from '../Figure';
 import RichText from '../RichText';
 import VenuesSection from '../VenuesSection';
 import SponsorsSection from '../SponsorsSection';
@@ -9,15 +8,6 @@ import TextAndImage from '../TextAndImage';
 import QuestionAndAnswerCollection from '../QuestionAndAnswerCollection';
 import Speakers from '../Speakers';
 import Sessions from '../../Sessions';
-
-const Figure = ({ value: { alt, asset } }) => (
-  <Image
-    src={imageUrlFor(asset).size(200, 200).url()}
-    width={200}
-    height={200}
-    alt={alt}
-  />
-);
 
 export const SharedSections = ({ value: { name, sections, ...rest } }) => (
   <>
@@ -27,7 +17,7 @@ export const SharedSections = ({ value: { name, sections, ...rest } }) => (
       // plus "figure" from /studio/schemas/documents/sharedSections.ts
       switch (section._type) {
         case 'figure':
-          return <Figure key={section._key} value={section} />;
+          return <Figure key={section._key} value={section} {...({} as any)} />;
         case 'articleSection':
           return <RichText key={section._key} value={section} />;
         case 'textAndImageSection':
