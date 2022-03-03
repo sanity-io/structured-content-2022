a806c50770c5bd97ea44d64ba0fb0b870ec14a30
Jonas Jensen <jonas.jensen@msn.com>
2022-03-04 00:56:55 +0100

    resolveProductionUrl.ts: use urlResolver.ts logic to get root URL

--- studio/src/resolveProductionUrl.ts
+++ studio/src/resolveProductionUrl.ts
@@ -1,14 +1,11 @@
-const remoteUrl = 'https://structured-content-2022.sanity.build/'
-const localUrl = 'http://localhost:3000'
+import { getEnvironment, rootURLs } from "./urlResolver";
 
 export default function resolveProductionUrl(doc) {
   if (doc._type !== 'route') {
     return null;
   }
 
-  const baseUrl = window.location.hostname === 'localhost' ? localUrl : remoteUrl;
-  const previewUrl = new URL(baseUrl);
-
+  const previewUrl = new URL(rootURLs[getEnvironment()].web);
   previewUrl.pathname = `/api/preview`;
   previewUrl.searchParams.append(`secret`, process.env.SANITY_STUDIO_PREVIEW_SECRET);
   previewUrl.searchParams.append(`slug`, doc?.slug?.current ?? `/`);
