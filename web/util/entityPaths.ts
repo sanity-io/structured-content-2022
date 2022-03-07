71b9b7b442c82a4dda42eda7ca7bc5d861109f2f
Jonas Jensen <jonas.jensen@msn.com>
2022-03-07 12:10:50 +0100

    entityPaths.ts: update getEntityPath to resolve route, article links
    These are the only two entities for which we support internal linking.

--- web/util/entityPaths.ts
+++ web/util/entityPaths.ts
@@ -1,23 +1,19 @@
 import urlJoin from 'proper-url-join';
-import { Session } from '../types/Session';
-import { Venue } from '../types/Venue';
-import { Person } from '../types/Person';
-
-type Entity = Person | Session | Venue;
 
 export const mainEventId = 'aad77280-6394-4090-afad-1c0f2a0416c6';
 
-export const getEntityPath = (entity?: Entity) => {
+export const getEntityPath = (entity?: any) => {
   if (!entity?.slug?.current) {
     return '#';
   }
 
   switch (entity._type) {
-    case 'session':
-    case 'person':
-    case 'venue':
-      return urlJoin(`${entity._type}s`, entity.slug?.current);
+    case 'route':
+      return urlJoin(entity.slug.current, { leadingSlash: true });
+    case 'article':
+      return urlJoin('article', entity.slug.current, { leadingSlash: true });
     default:
-      return '';
+      console.error(`getEntityPath: unsupported entity type: '${entity._type}'`);
+      return '#';
   }
 };
