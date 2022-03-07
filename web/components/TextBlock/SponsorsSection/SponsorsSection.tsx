c18c4666096d2aa6f83fff159109c21a54538bd6
Jonas Jensen <jonas.jensen@msn.com>
2022-03-07 10:54:07 +0100

    SponsorsSection.tsx: remove unused import

--- web/components/TextBlock/SponsorsSection/SponsorsSection.tsx
+++ web/components/TextBlock/SponsorsSection/SponsorsSection.tsx
@@ -4,7 +4,6 @@ import { Sponsorship } from '../../../types/Sponsorship';
 import { EntitySectionSelection } from '../../../types/EntitySectionSelection';
 import Sponsor from '../../Sponsor';
 import GridWrapper from '../../GridWrapper';
-import Heading from '../../Heading';
 import styles from './SponsorsSection.module.css';
 
 type SponsorsSectionProps = {
