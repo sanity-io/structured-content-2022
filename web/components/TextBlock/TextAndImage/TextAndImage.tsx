4cf3581a74a6785b4eca9c5413cf3ddc85520678
Jonas Jensen <jonas.jensen@msn.com>
2022-03-07 09:03:37 +0100

    Revert "TextAndImage.tsx: render Figure's alt text if present"
    This reverts commit bbaeb5eb2b7031639b7cc63fa249816a56116978, which was accidentally pushed to the staging branch without going through review first.

--- web/components/TextBlock/TextAndImage/TextAndImage.tsx
+++ web/components/TextBlock/TextAndImage/TextAndImage.tsx
@@ -1,14 +1,13 @@
 import { PortableTextComponentProps } from '@portabletext/react';
 import { imageUrlFor } from '../../../lib/sanity';
 import { Section } from '../../../types/Section';
-import { Figure } from "../../../types/Figure";
 import GridWrapper from '../../GridWrapper';
 import Heading from '../../Heading';
 import TextBlock from '../TextBlock';
 import styles from './TextAndImage.module.css';
 
 type TextAndImageProps = {
-  image: Figure;
+  image: any;
   text: Section[];
   tagline?: string;
   title?: string;
@@ -24,7 +23,7 @@ export const TextAndImage = ({
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src={imageUrlFor(image).ignoreImageParams().url()}
-            alt={image.alt}
+            alt=""
             className={styles.image}
           />
         </div>
