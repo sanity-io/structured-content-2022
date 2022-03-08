70759b41ff6b999e186ad48fa6207db05aad0d3f
Jonas Jensen <jonas.jensen@msn.com>
2022-03-08 14:43:45 +0100

    api/revalidate.js: separate query for route updates

--- web/pages/api/revalidate.ts
+++ web/pages/api/revalidate.ts
@@ -6,17 +6,28 @@ import client from '../../lib/sanity.server';
 const SECTION_UPDATED_QUERY = `
   *[_type == "page" && references($id)] {
     "slug": *[_type == "route" && references(^._id)].slug.current
-  }["slug"][]
-`;
-
+  }["slug"][]`;
 const PAGE_UPDATED_QUERY = `*[_type == "route" && references($id)].slug.current`;
-
-// TODO: ROUTE_UPDATED_QUERY?
+const ROUTE_UPDATED_QUERY = `*[_type == "route" && _id == $id].slug.current`;
 
 type Body = {
   _id: string;
   _type: string;
 }
+
+const getQueryForType = (type: string) => {
+  switch (type) {
+    case 'route':
+      return ROUTE_UPDATED_QUERY;
+    case 'page':
+      return PAGE_UPDATED_QUERY;
+    default:
+      return SECTION_UPDATED_QUERY;
+  }
+};
+
+const log = (msg: string, error?: boolean) =>
+  console[error ? "error" : "log"](`[revalidate] ${msg}`);
 
 export default async function revalidate(
   req: NextApiRequest,
@@ -24,22 +35,22 @@ export default async function revalidate(
 ) {
   if (!isValidRequest(req, process.env.SANITY_STUDIO_REVALIDATE_SECRET)) {
     const invalidRequest = 'Invalid request'
-    console.error(invalidRequest);
-    res.status(401).json({ message: invalidRequest });
-    return;
+    log(invalidRequest, true);
+    return res.status(401).json({ message: invalidRequest });
   }
 
   const { _id: id, _type } = req.body as Body;
   if (typeof id !== 'string' || !id) {
     const invalidId = 'Invalid _id';
-    console.error(invalidId);
+    log(invalidId, true);
     return res.status(401).json({ message: invalidId });
   }
 
-  const query = _type === 'page' ? PAGE_UPDATED_QUERY : SECTION_UPDATED_QUERY;
-  const relatedRoutes = await client.fetch(query, { id });
+  log(`Querying for type '${_type}' ..`);
+  const relatedRoutes = await client.fetch(getQueryForType(_type), { id });
   if (!Array.isArray(relatedRoutes) || !relatedRoutes.length) {
     const noUpdatedObjects = 'No pages reference updated object';
+    log(noUpdatedObjects, true);
     return res.status(401).json({ message: noUpdatedObjects });
   }
 
@@ -48,9 +59,10 @@ export default async function revalidate(
       relatedRoutes.map((route) => res.unstable_revalidate(urlJoin(route)))
     );
     const updatedRoutes = `Updated routes: ${relatedRoutes.join(', ')}`;
-    console.log(updatedRoutes);
+    log(updatedRoutes);
     return res.status(200).json({ message: updatedRoutes });
   } catch (err) {
+    log(err.message, true);
     return res.status(500).json({ message: err.message });
   }
 }
