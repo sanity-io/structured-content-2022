08f7468b5c7b56280d8ba39e32c426af63583615
Jonas Jensen <jonas.jensen@msn.com>
2022-03-08 14:17:56 +0100

    WIP: api/revalidate.js: separate query for page updates

--- web/pages/api/revalidate.ts
+++ web/pages/api/revalidate.ts
@@ -1,28 +1,43 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import urlJoin from 'proper-url-join';
-import { assertValidRequest } from '@sanity/webhook';
+import { isValidRequest } from '@sanity/webhook';
 import client from '../../lib/sanity.server';
 
-const QUERY = `
+const SECTION_UPDATED_QUERY = `
   *[_type == "page" && references($id)] {
     "slug": *[_type == "route" && references(^._id)].slug.current
   }["slug"][]
 `;
 
+const PAGE_UPDATED_QUERY = `*[_type == "route" && references($id)].slug.current`;
+
+// TODO: ROUTE_UPDATED_QUERY?
+
+type Body = {
+  _id: string;
+  _type: string;
+}
+
 export default async function revalidate(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  assertValidRequest(req, process.env.SANITY_STUDIO_REVALIDATE_SECRET);
+  if (!isValidRequest(req, process.env.SANITY_STUDIO_REVALIDATE_SECRET)) {
+    const invalidRequest = 'Invalid request'
+    console.error(invalidRequest);
+    res.status(401).json({ message: invalidRequest });
+    return;
+  }
 
-  const { _id: id } = req.body;
+  const { _id: id, _type } = req.body as Body;
   if (typeof id !== 'string' || !id) {
     const invalidId = 'Invalid _id';
     console.error(invalidId);
     return res.status(401).json({ message: invalidId });
   }
 
-  const relatedRoutes = await client.fetch(QUERY, { id });
+  const query = _type === 'page' ? PAGE_UPDATED_QUERY : SECTION_UPDATED_QUERY;
+  const relatedRoutes = await client.fetch(query, { id });
   if (!Array.isArray(relatedRoutes) || !relatedRoutes.length) {
     const noUpdatedObjects = 'No pages reference updated object';
     return res.status(401).json({ message: noUpdatedObjects });
