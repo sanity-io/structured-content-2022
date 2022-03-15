2c0a3e941af0a1cc618af03d08d5f988b828cd43
Jonas Jensen <jonas.jensen@msn.com>
2022-03-15 12:21:05 +0100

    Nav: slide in mobile menu items one-by-one

--- web/components/Nav/Nav.tsx
+++ web/components/Nav/Nav.tsx
@@ -1,9 +1,11 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import useInterval from 'use-interval';
+import logo from '../../images/logo.svg';
 import ButtonLink from '../ButtonLink';
 import GridWrapper from '../GridWrapper';
-import logo from '../../images/logo.svg';
+import MenuItem from './MenuItem';
 import styles from './Nav.module.css';
 
 interface NavProps {
@@ -15,34 +17,55 @@ interface NavProps {
 export const Nav = ({ onFrontPage, currentPath, ticketsUrl }: NavProps) => {
   const [menuOpened, setMenuOpened] = useState(false);
   const contentsId = 'nav-menu-contents';
+  const [menuItemsCount, setMenuItemsCount] = useState(0);
 
   useEffect(() => {
     document.body.classList.toggle('main-menu-open', menuOpened);
+    if (!menuOpened) {
+      setMenuItemsCount(0);
+    }
   }, [menuOpened]);
 
   const toggleMenu = () => setMenuOpened(!menuOpened);
   const closeMenu = () => setMenuOpened(false);
 
-  const BasicMenuItem = ({
-    urlPath,
-    label,
-  }: {
-    urlPath: string;
-    label: string;
-  }) => (
-    <li>
-      <Link href={urlPath}>
-        <a
-          className={clsx(
-            styles.link,
-            urlPath === currentPath && styles.current
-          )}
-          onClick={closeMenu}
-        >
-          {label}
-        </a>
-      </Link>
-    </li>
+  const menuItems = [
+    <MenuItem
+      key={1}
+      {...{ currentPath, closeMenu }}
+      href={ticketsUrl}
+      label="Tickets"
+      className={styles.ticketItem}
+    />,
+    <MenuItem
+      key={2}
+      {...{ currentPath, closeMenu }}
+      href="/program"
+      label="Program"
+    />,
+    <MenuItem
+      key={3}
+      {...{ currentPath, closeMenu }}
+      href="/sponsorship-information"
+      label="Sponsorship"
+    />,
+    <MenuItem
+      key={4}
+      {...{ currentPath, closeMenu }}
+      href="/registration-info"
+      label="Registration info"
+    />,
+    <MenuItem
+      key={5}
+      {...{ currentPath, closeMenu }}
+      href="/about"
+      label="About"
+    />,
+  ];
+
+  useInterval(
+    () => setMenuItemsCount(menuItemsCount + 1),
+    menuOpened && menuItemsCount < menuItems.length ? 100 : null
   );
 
   return (
@@ -81,23 +104,7 @@ export const Nav = ({ onFrontPage, currentPath, ticketsUrl }: NavProps) => {
             </a>
           </Link>
           <ul className={clsx(styles.items, !menuOpened && styles.menuClosed)}>
-            <li className={styles.ticketItem}>
-              <Link href={ticketsUrl}>
-                <a className={styles.link} onClick={closeMenu}>
-                  Tickets
-                </a>
-              </Link>
-            </li>
-            <BasicMenuItem urlPath="/program" label="Program" />
-            <BasicMenuItem
-              urlPath="/sponsorship-information"
-              label="Sponsorship"
-            />
-            <BasicMenuItem
-              urlPath="/registration-info"
-              label="Registration info"
-            />
-            <BasicMenuItem urlPath="/about" label="About" />
+            {menuItems.slice(0, menuItemsCount)}
           </ul>
           <div className={styles.ticketButton}>
             <ButtonLink url={ticketsUrl} text="Tickets" />
