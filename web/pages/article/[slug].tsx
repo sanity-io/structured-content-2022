4ce87c78b2bfe3f8cfe7263529cebadd35013344
Jonas Jensen <jonas.jensen@msn.com>
2022-03-16 15:35:52 +0100

    Editorial articles: render authors and related entities

--- web/pages/article/[slug].tsx
+++ web/pages/article/[slug].tsx
@@ -1,7 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { groq } from 'next-sanity';
 import urlJoin from 'proper-url-join';
-import Card from "../../components/Card";
+import Card from '../../components/Card';
 import Hero from '../../components/Hero';
 import Footer from '../../components/Footer';
 import GridWrapper from '../../components/GridWrapper';
@@ -9,6 +9,7 @@ import MetaTags from '../../components/MetaTags';
 import Nav from '../../components/Nav';
 import TextBlock from '../../components/TextBlock';
 import client from '../../lib/sanity.server';
+import { formatDate } from '../../util/date';
 import { mainEventId } from '../../util/entityPaths';
 import { BLOCK_CONTENT } from '../../util/queries';
 import { Article } from '../../types/Article';
@@ -19,7 +20,9 @@ import articleStyles from './article.module.css';
 const QUERY = groq`
   {
     "article": *[_type == "article" && slug.current == $slug][0] {
+      _createdAt,
       _id,
+      _updatedAt,
       heading,
       summary,
       content[] { ${BLOCK_CONTENT} },
@@ -63,68 +66,106 @@ interface ArticleRouteProps {
 
 const ArticleRoute = ({
   data: {
-    article: { heading, summary, content, relatedTo: { people, sessions, venues } },
+    article: {
+      _createdAt,
+      _updatedAt,
+      heading,
+      summary,
+      content,
+      authors,
+      relatedTo,
+    },
     home: { ticketsUrl },
     footer,
     rewrittenArticleSlugs,
   },
   slug,
-}: ArticleRouteProps) => (
-  <>
-    <MetaTags
-      title={heading}
-      description={summary}
-      currentPath={slug}
-      rewrittenArticleSlugs={rewrittenArticleSlugs}
-    />
-    <header className={styles.header}>
-      <Nav
-        onFrontPage={false}
-        currentPath={`/article/${slug}`}
-        ticketsUrl={ticketsUrl}
+}: ArticleRouteProps) => {
+  const { people, sessions, venues } = relatedTo || {};
+  return (
+    <>
+      <MetaTags
+        title={heading}
+        description={summary}
+        currentPath={slug}
+        rewrittenArticleSlugs={rewrittenArticleSlugs}
       />
-    </header>
-    <main>
-      <Hero heading={heading} summary={summary} />
-      {/* This is a workaround for TextBlock and the types it handles currently
-       * being tied to PortableText. Ideally we would just reuse the RichText
-       * component here instead of copy/pasting the wrappers/CSS.
-       */}
-      <GridWrapper>
-        <div className={articleStyles.container}>
-          <div className={articleStyles.content}>
-
+      <header className={styles.header}>
+        <Nav
+          onFrontPage={false}
+          currentPath={`/article/${slug}`}
+          ticketsUrl={ticketsUrl}
+        />
+      </header>
+      <main>
+        <Hero heading={heading} summary={summary}>
+          <div className={articleStyles.extraHeroContent}>
             <div>
-              <TextBlock value={content} />
-
-              <div>
-                <h3>Related Sessions</h3>
-                {sessions.map((title, index) => (
-                  <Card key={`${title}_${index}`}>{title}</Card>
+              {Array.isArray(authors) &&
+                authors.map(({ name, photo }) => (
+                  <Card key={name} figure={photo}>
+                    {name}
+                  </Card>
                 ))}
-              </div>
-
+            </div>
+            <div className={articleStyles.timestamps}>
+              <div>Published on: {formatDate(_createdAt, 'UTC')}</div>
+              {_updatedAt && (
+                <div className={articleStyles.pipe}>&nbsp;|&nbsp;</div>
+              )}
+              {_updatedAt && (
+                <div>Updated on: {formatDate(_updatedAt, 'UTC')}</div>
+              )}
+            </div>
+          </div>
+        </Hero>
+        {/* This is a workaround for TextBlock and the types it handles currently
+         * being tied to PortableText. Ideally we would just reuse the RichText
+         * component here instead of copy/pasting the wrappers/CSS.
+         */}
+        <GridWrapper>
+          <div className={articleStyles.container}>
+            <div className={articleStyles.content}>
               <div>
-                <h3>Related people</h3>
-                {people.map(({ name, photo }, index) => (
-                  <Card key={`${name}_${index}`} figure={photo}>{name}</Card>
-                ))}
-              </div>
+                <TextBlock value={content} />
 
-              <div>
-                <h3>Related venues</h3>
-                {venues.map((name, index) => (
-                  <Card key={`${name}_${index}`}>{name}</Card>
-                ))}
+                {Array.isArray(sessions) && sessions.length && (
+                  <div>
+                    <h3>Related Sessions</h3>
+                    {sessions.map((title, index) => (
+                      <Card key={`${title}_${index}`}>{title}</Card>
+                    ))}
+                  </div>
+                )}
+
+                {Array.isArray(people) && people.length && (
+                  <div>
+                    <h3>Related people</h3>
+                    {people.map(({ name, photo }, index) => (
+                      <Card key={`${name}_${index}`} figure={photo}>
+                        {name}
+                      </Card>
+                    ))}
+                  </div>
+                )}
+
+                {Array.isArray(venues) && venues.length && (
+                  <div>
+                    <h3>Related venues</h3>
+                    {venues.map((name, index) => (
+                      <Card key={`${name}_${index}`}>{name}</Card>
+                    ))}
+                  </div>
+                )}
               </div>
             </div>
           </div>
-        </div>
-      </GridWrapper>
-    </main>
-    <Footer links={footer.links} />
-  </>
-);
+        </GridWrapper>
+      </main>
+      <Footer links={footer.links} />
+    </>
+  );
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const allSlugsQuery = groq`*[defined(slug.current) && _type == 'article'][].slug.current`;
--- web/types/Article.ts
+++ web/types/Article.ts
@@ -1,21 +1,22 @@
 import { Slug } from './Slug';
 import { Section } from './Section';
-import { Figure } from "./Figure";
+import { Figure } from './Figure';
 
 export type Article = {
+  _createdAt: string;
   _id: string;
   _rev: string;
   _type: 'article';
-  authors?: { name: string, photo: Figure }[];
+  _updatedAt: string;
+  authors?: { name: string; photo: Figure }[];
   content: Section[];
   heading: string;
   publishedAt: string;
   slug: Slug;
   summary: string;
-  _updatedAt: string;
   relatedTo?: {
     people: { name: string; photo: Figure }[];
     sessions: string[];
     venues: string[];
-  }
+  };
 };
