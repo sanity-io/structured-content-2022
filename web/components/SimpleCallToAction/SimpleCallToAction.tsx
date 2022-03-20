0c4f951b566dd171cec8a7cfc421f806c372afa3
Knut Melvær <knut@sanity.io>
2022-03-20 16:59:45 -0700

    Update web/components/SimpleCallToAction/SimpleCallToAction.tsx
    Co-authored-by: Jonas Jensen <jonas.jensen@msn.com>

--- web/components/SimpleCallToAction/SimpleCallToAction.tsx
+++ web/components/SimpleCallToAction/SimpleCallToAction.tsx
@@ -4,13 +4,19 @@ import urlJoin from 'proper-url-join';
 
 export const SimpleCallToAction = ({ text, link }: SimpleCallToActionProps) => {
   const openInNewTab = Boolean(link?.blank);
-  return text && link?.internal?.slug?.current ? (
-    <ButtonLink
-      text={text}
-      url={urlJoin(link.internal.slug.current)}
-      openInNewTab={openInNewTab}
-    />
-  ) : text && link?.external ? (
-    <ButtonLink text={text} url={link.external} openInNewTab={openInNewTab} />
-  ) : null;
+  if (text && link?.internal?.slug?.current) {
+    return (
+      <ButtonLink
+        text={text}
+        url={urlJoin(link.internal.slug.current)}
+        openInNewTab={openInNewTab}
+      />
+    );
+  }
+  if (text && link?.external) {
+    return (
+      <ButtonLink text={text} url={link.external} openInNewTab={openInNewTab} />
+    );
+  }
+  return null;
 };
