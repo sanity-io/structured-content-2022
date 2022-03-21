28e906ba531c9b2cf75a96b93870395e9ae65873
Jonas Jensen <jonas.jensen@msn.com>
2022-03-21 11:14:55 +0100

    Nav: useInterval -> animationDelay props
    Good simplification of the code thanks to @oyvind-stenhaug's suggestion in https://github.com/sanity-io/structured-content-2022/pull/114\#discussion_r827832789. Removes the need for the use-interval module, which is uninstalled again in this commit.

--- web/components/Nav/Nav.tsx
+++ web/components/Nav/Nav.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import useInterval from 'use-interval';
 import logo from '../../images/logo.svg';
 import ButtonLink from '../ButtonLink';
 import GridWrapper from '../GridWrapper';
@@ -17,57 +16,14 @@ interface NavProps {
 export const Nav = ({ onFrontPage, currentPath, ticketsUrl }: NavProps) => {
   const [menuOpened, setMenuOpened] = useState(false);
   const contentsId = 'nav-menu-contents';
-  const [menuItemsCount, setMenuItemsCount] = useState(0);
 
   useEffect(() => {
     document.body.classList.toggle('main-menu-open', menuOpened);
-    if (!menuOpened) {
-      setMenuItemsCount(0);
-    }
   }, [menuOpened]);
 
   const toggleMenu = () => setMenuOpened(!menuOpened);
   const closeMenu = () => setMenuOpened(false);
 
-  const menuItems = [
-    <MenuItem
-      key={1}
-      {...{ currentPath, closeMenu }}
-      href={ticketsUrl}
-      target="_blank"
-      rel="noreferrer"
-      className={styles.ticketItem}
-    >
-      Tickets
-    </MenuItem>,
-    <MenuItem key={2} {...{ currentPath, closeMenu }} href="/program">
-      Program
-    </MenuItem>,
-    <MenuItem
-      key={3}
-      {...{ currentPath, closeMenu }}
-      href="/sponsorship-information"
-    >
-      Sponsorship
-    </MenuItem>,
-    <MenuItem key={4} {...{ currentPath, closeMenu }} href="/registration-info">
-      Registration
-    </MenuItem>,
-    <MenuItem key={5} {...{ currentPath, closeMenu }} href="/about">
-      About
-    </MenuItem>,
-  ];
-
-  const isMenuAnimationActive = menuOpened && menuItemsCount < menuItems.length;
-  useInterval(
-    () => setMenuItemsCount(menuItemsCount + 1),
-    isMenuAnimationActive ? 50 : null,
-    isMenuAnimationActive
-  );
-
-  const isMdUp =
-    typeof window !== 'undefined' &&
-    window.matchMedia('(min-width: 768px)').matches;
   return (
     <nav className={clsx(styles.nav, onFrontPage && styles.onFrontPage)}>
       <GridWrapper>
@@ -104,7 +60,43 @@ export const Nav = ({ onFrontPage, currentPath, ticketsUrl }: NavProps) => {
             </a>
           </Link>
           <ul className={clsx(styles.items, !menuOpened && styles.menuClosed)}>
-            {isMdUp ? menuItems : menuItems.slice(0, menuItemsCount)}
+            <MenuItem
+              {...{ currentPath, closeMenu }}
+              href={ticketsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.ticketItem}
+            >
+              Tickets
+            </MenuItem>
+            <MenuItem
+              {...{ currentPath, closeMenu }}
+              href="/program"
+              style={{ animationDelay: '50ms' }}
+            >
+              Program
+            </MenuItem>
+            <MenuItem
+              {...{ currentPath, closeMenu }}
+              href="/sponsorship-information"
+              style={{ animationDelay: '100ms' }}
+            >
+              Sponsorship
+            </MenuItem>
+            <MenuItem
+              {...{ currentPath, closeMenu }}
+              href="/registration-info"
+              style={{ animationDelay: '150ms' }}
+            >
+              Registration
+            </MenuItem>
+            <MenuItem
+              {...{ currentPath, closeMenu }}
+              href="/about"
+              style={{ animationDelay: '200ms' }}
+            >
+              About
+            </MenuItem>
           </ul>
           <div className={styles.ticketButton}>
             <ButtonLink url={ticketsUrl} text="Tickets" openInNewTab={true} />
