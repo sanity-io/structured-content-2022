bedd47a75ee4c048cca23fcebbf1fb52ccee0699
Jonas Jensen <jonas.jensen@msn.com>
2022-03-23 12:01:02 +0100

    animation.ts: reduce --delay max value

--- web/util/animation.ts
+++ web/util/animation.ts
@@ -7,6 +7,6 @@ export const animationProperties = (randomize?: boolean) =>
   ({
     '--distance': randomize ? `${randomRange(0, 150)}px` : '32px',
     '--duration': randomize ? `${randomRange(300, 500)}ms` : '1200ms',
-    '--delay': `${randomRange(100, 650)}ms`, // Delay is always random
+    '--delay': `${randomRange(100, 450)}ms`, // Delay is always random
     '--rotation': randomize ? `${randomRange(-16, 16)}deg` : 0,
   } as CSSProperties);
