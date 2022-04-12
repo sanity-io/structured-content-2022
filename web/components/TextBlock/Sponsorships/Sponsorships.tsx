90d89ef4ccdf9f8d7ddabad84957bcc57ef54f0a
Jon Eide Johnsen <joneidejohnsen@gmail.com>
2022-04-12 17:02:20 +0200

    feat: add heading to sponsorships section

--- web/components/TextBlock/Sponsorships/Sponsorships.tsx
+++ web/components/TextBlock/Sponsorships/Sponsorships.tsx
@@ -10,6 +10,7 @@ import styles from './Sponsorships.module.css';
 interface SponsorshipsProps {
   value: {
     type: EntitySectionSelection;
+    heading: string;
     allSponsorships?: Sponsorship[];
     sponsorships?: Sponsorship[];
   };
@@ -25,7 +26,12 @@ const allBenefitNamesSortOrderPreserved = (allSponsorships: Sponsorship[]) => {
 };
 
 export const Sponsorships = ({
-  value: { type, sponsorships: highlightedSponsorships, allSponsorships },
+  value: {
+    type,
+    heading,
+    sponsorships: highlightedSponsorships,
+    allSponsorships,
+  },
 }: SponsorshipsProps) => {
   if (type === 'none') {
     return null;
@@ -44,40 +50,43 @@ export const Sponsorships = ({
 
     return (
       <GridWrapper>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th />
-              {sponsorships.map((s) => (
-                <th key={s._id} scope="col" className={styles.columnHeader}>
-                  <strong className={styles.sponsorshipName}>{s.type}</strong>
-                  <div className={styles.price}>{formatPrice(s.price)}</div>
-                </th>
+        <div className={styles.container}>
+          {heading && <h2 className={styles.heading}>{heading}</h2>}
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th />
+                {sponsorships.map((s) => (
+                  <th key={s._id} scope="col" className={styles.columnHeader}>
+                    <strong className={styles.sponsorshipName}>{s.type}</strong>
+                    <div className={styles.price}>{formatPrice(s.price)}</div>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {allBenefitNamesSortOrderPreserved(allSponsorships).map((b) => (
+                <BenefitRow key={b} name={b} sponsorships={sponsorships} />
               ))}
-            </tr>
-          </thead>
-          <tbody>
-            {allBenefitNamesSortOrderPreserved(allSponsorships).map((b) => (
-              <BenefitRow key={b} name={b} sponsorships={sponsorships} />
-            ))}
-          </tbody>
-        </table>
+            </tbody>
+          </table>
 
-        <div className={styles.listContainer}>
-          {sponsorships.map((s) => {
-            const features = s.benefits?.map(
-              ({ number, benefit, description }) =>
-                (typeof number === 'number' ? `${number} ` : '') +
-                benefit.name +
-                (description ? ` – ${description}` : '')
-            );
-            return (
-              <FeatureSection features={features} key={s._id}>
-                <h3 className={styles.sponsorshipName}>{s.type}</h3>
-                <div className={styles.price}>{formatPrice(s.price)}</div>
-              </FeatureSection>
-            );
-          })}
+          <div className={styles.listContainer}>
+            {sponsorships.map((s) => {
+              const features = s.benefits?.map(
+                ({ number, benefit, description }) =>
+                  (typeof number === 'number' ? `${number} ` : '') +
+                  benefit.name +
+                  (description ? ` – ${description}` : '')
+              );
+              return (
+                <FeatureSection features={features} key={s._id}>
+                  <h3 className={styles.sponsorshipName}>{s.type}</h3>
+                  <div className={styles.price}>{formatPrice(s.price)}</div>
+                </FeatureSection>
+              );
+            })}
+          </div>
         </div>
       </GridWrapper>
     );
