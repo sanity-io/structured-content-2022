17278ebcd3aecaa2c3c5191d82bc7f44a805868c
Jonas Jensen <jonas.jensen@msn.com>
2022-04-22 15:30:20 +0200

    session.ts: extract getDuration as separate function

--- web/types/Program.ts
+++ web/types/Program.ts
@@ -1,16 +1,18 @@
 import { Session } from './Session';
 import { Venue } from './Venue';
 
+export type ProgramSession = {
+  duration?: number;
+  durationOverride?: number;
+  _key: string;
+  _type: 'padding' | 'slot';
+  session?: Session;
+};
+
 export type Program = {
   _id: string;
   internalName: string;
-  sessions: {
-    duration?: number;
-    durationOverride?: number;
-    _key: string;
-    _type: 'padding' | 'slot';
-    session?: Session;
-  }[];
+  sessions: ProgramSession[];
   startDateTime: string;
   venues: Venue[];
 };
--- web/util/session.ts
+++ web/util/session.ts
@@ -1,6 +1,6 @@
 import { addMinutes, parseISO } from 'date-fns';
 import type { Session } from '../types/Session';
-import type { Program } from '../types/Program';
+import type { Program, ProgramSession } from '../types/Program';
 import {
   formatDateWithDay,
   formatTimeRange,
@@ -32,6 +32,9 @@ export const sessionStart = (
   return addMinutes(start, sessionStartOffset);
 };
 
+const getDuration = ({ session, duration, durationOverride }: ProgramSession) =>
+  durationOverride ?? duration ?? session?.duration ?? 0;
+
 /* Given all Programs, finds the Programs that contain the Session (matching on _id).
  * For each match, returns:
  * - the Program's associated Venue name (using the first entry in the Program's Venues array)
@@ -50,25 +53,20 @@ export const sessionTimingDetailsForMatchingPrograms = (
     }))
     .filter((entry) => Boolean(entry.session))
     .map(({ program: { sessions, startDateTime, venues }, session }) => {
-      const [{ name, timezone }] = venues;
       const start = sessionStart(
         startDateTime,
         session.session._id,
-        sessions.map(({ session, duration, durationOverride }) => ({
-          ...session,
-          duration: durationOverride ?? duration ?? session?.duration ?? 0,
+        sessions.map((programSession) => ({
+          ...programSession.session,
+          duration: getDuration(programSession),
         }))
       );
+
+      const [{ name, timezone }] = venues;
       return {
         label: name,
         date: formatDateWithDay(parseISO(startDateTime), timezone, ', '),
-        time: formatTimeRange(
-          start,
-          session.durationOverride ??
-            session.duration ??
-            session.session.duration,
-          timezone
-        ),
+        time: formatTimeRange(start, getDuration(session), timezone),
         timezone: getNonLocationTimezone(start, timezone, true),
       };
     });
