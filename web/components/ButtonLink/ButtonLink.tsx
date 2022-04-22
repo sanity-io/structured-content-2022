8fef6195304915010a82ebd589080ca8a1bcbcd5
Jonas Jensen <jonas.jensen@msn.com>
2022-04-22 12:50:55 +0200

    WIP: ButtonLink.tsx: center CTAs on front page

--- web/components/ButtonLink/ButtonLink.tsx
+++ web/components/ButtonLink/ButtonLink.tsx
@@ -1,4 +1,6 @@
+import clsx from 'clsx';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './ButtonLink.module.css';
 
 interface ButtonLinkProps {
@@ -7,13 +9,24 @@ interface ButtonLinkProps {
   openInNewTab?: boolean;
 }
 
-export const ButtonLink = ({ text, url, openInNewTab }: ButtonLinkProps) =>
-  openInNewTab ? (
-    <a className={styles.button} href={url} target="_blank" rel="noreferrer">
-      {text}
-    </a>
-  ) : (
-    <Link href={url}>
-      <a className={styles.button}>{text}</a>
-    </Link>
+export const ButtonLink = ({ text, url, openInNewTab }: ButtonLinkProps) => {
+  const router = useRouter();
+  return (
+    <div className={clsx(router.asPath === '/' && styles.container)}>
+      {openInNewTab ? (
+        <a
+          className={styles.button}
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {text}
+        </a>
+      ) : (
+        <Link href={url}>
+          <a className={styles.button}>{text}</a>
+        </Link>
+      )}
+    </div>
   );
+};
