d634da6d14b142a4684dd538be6cd87dc6bf42aa
Jonas Jensen <jonas.jensen@msn.com>
2022-04-22 13:03:55 +0200

    Revert "WIP: ButtonLink.tsx: center CTAs on front page"
    This reverts commit 8fef6195304915010a82ebd589080ca8a1bcbcd5. Turns out, this styling tweak should only be applied for a select few sections. https://wja.slack.com/archives/C02E2ERKR7E/p1650625062998349?thread_ts=1650622084.594009&cid=C02E2ERKR7E

--- web/components/ButtonLink/ButtonLink.tsx
+++ web/components/ButtonLink/ButtonLink.tsx
@@ -1,6 +1,4 @@
-import clsx from 'clsx';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import styles from './ButtonLink.module.css';
 
 interface ButtonLinkProps {
@@ -9,24 +7,13 @@ interface ButtonLinkProps {
   openInNewTab?: boolean;
 }
 
-export const ButtonLink = ({ text, url, openInNewTab }: ButtonLinkProps) => {
-  const router = useRouter();
-  return (
-    <div className={clsx(router.asPath === '/' && styles.container)}>
-      {openInNewTab ? (
-        <a
-          className={styles.button}
-          href={url}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {text}
-        </a>
-      ) : (
-        <Link href={url}>
-          <a className={styles.button}>{text}</a>
-        </Link>
-      )}
-    </div>
+export const ButtonLink = ({ text, url, openInNewTab }: ButtonLinkProps) =>
+  openInNewTab ? (
+    <a className={styles.button} href={url} target="_blank" rel="noreferrer">
+      {text}
+    </a>
+  ) : (
+    <Link href={url}>
+      <a className={styles.button}>{text}</a>
+    </Link>
   );
-};
