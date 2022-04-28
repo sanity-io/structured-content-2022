330ae7a8de25367700f6b78853ed506c5f4fa553
Jonas Jensen <jonas.jensen@msn.com>
2022-04-28 18:54:39 +0200

    api/og-image/[slug].ts: attempt to inlude .png font image in lambda

--- web/pages/api/og-image/[slug].ts
+++ web/pages/api/og-image/[slug].ts
@@ -7,6 +7,10 @@ export default async function ogImage(req: NextApiRequest, res: NextApiResponse)
   // Resolves to root of /web directory, __dirname is buggy in Next.js
   const imagePath = resolve('images', 'og_image_background.png');
   const image = await Jimp.read(imagePath);
+
+  const pngFontPath = resolve('public', 'static', 'fonts', 'national-2-narrow-bold.png');
+  console.log(pngFontPath); // Make Vercel include this file (referenced by .fnt below) in lambda
+
   const font = await Jimp.loadFont(resolve('public', 'static', 'fonts', 'national-2-narrow-bold.fnt'));
   wrap(req.query.slug as string, { width: 26 }).split('\n').forEach((line, i) => {
     image.print(font, 32, 40 + i * 80, line);
