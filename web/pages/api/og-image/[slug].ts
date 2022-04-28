e4ba6ec8a30e112eb7202d2d4d2ad18f244b4639
Jonas Jensen <jonas.jensen@msn.com>
2022-04-28 18:42:54 +0200

    api/og-image/[slug].ts: correct font and word-wrapping

--- web/pages/api/og-image/[slug].ts
+++ web/pages/api/og-image/[slug].ts
@@ -1,14 +1,16 @@
 import Jimp from 'jimp-compact';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { resolve } from "path";
+import wrap from "word-wrap";
 
 export default async function ogImage(req: NextApiRequest, res: NextApiResponse) {
   // Resolves to root of /web directory, __dirname is buggy in Next.js
   const imagePath = resolve('images', 'og_image_background.png');
   const image = await Jimp.read(imagePath);
-  //const font = await Jimp.loadFont(resolve('public', 'static', 'fonts', 'national-2-narrow-bold.woff2'));
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  image.print(font, 10, 10, req.query.slug);
+  const font = await Jimp.loadFont(resolve('public', 'static', 'fonts', 'national-2-narrow-bold.fnt'));
+  wrap(req.query.slug as string, { width: 26 }).split('\n').forEach((line, i) => {
+    image.print(font, 32, 40 + i * 80, line);
+  });
 
   const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
   res.writeHead(200, {
