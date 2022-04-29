0972639ff1de2f9d00bc79b01515b16cc8db6d2d
Jonas Jensen <jonas.jensen@msn.com>
2022-04-29 09:30:41 +0200

    api/og-image/[slug].ts: try-catch & try importing font png

--- web/pages/api/og-image/[slug].ts
+++ web/pages/api/og-image/[slug].ts
@@ -1,25 +1,41 @@
 import Jimp from 'jimp-compact';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { resolve } from "path";
-import wrap from "word-wrap";
+import { resolve } from 'path';
+import wrap from 'word-wrap';
+// Make Vercel include this file (referenced by national-2-narrow-bold.fnt) in lambda
+import '../../../public/static/fonts/national-2-narrow-bold-min.png';
 
-export default async function ogImage(req: NextApiRequest, res: NextApiResponse) {
-  // Resolves to root of /web directory, __dirname is buggy in Next.js
-  const imagePath = resolve('images', 'og_image_background.png');
-  const image = await Jimp.read(imagePath);
+const FONT_SIZE = 80;
+const PADDING_X = 32;
+const PADDING_Y = 40;
+const WORD_WRAP_LIMIT = 26;
 
-  const pngFontPath = resolve('public', 'static', 'fonts', 'national-2-narrow-bold.png');
-  console.log(pngFontPath); // Make Vercel include this file (referenced by .fnt below) in lambda
+export default async function ogImage(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    // Resolves to root of /web directory, __dirname is buggy in Next.js
+    const imagePath = resolve('images', 'og_image_background.png');
+    const image = await Jimp.read(imagePath);
 
-  const font = await Jimp.loadFont(resolve('public', 'static', 'fonts', 'national-2-narrow-bold.fnt'));
-  wrap(req.query.slug as string, { width: 26 }).split('\n').forEach((line, i) => {
-    image.print(font, 32, 40 + i * 80, line);
-  });
+    const font = await Jimp.loadFont(
+      resolve('public', 'static', 'fonts', 'national-2-narrow-bold.fnt')
+    );
+    wrap(req.query.slug as string, { width: WORD_WRAP_LIMIT })
+      .split('\n')
+      .forEach((line, i) =>
+        image.print(font, PADDING_X, PADDING_Y + i * FONT_SIZE, line)
+      );
 
-  const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
-  res.writeHead(200, {
-    'Content-Type': 'image/png',
-    'Content-Length': buffer.length,
-  });
-  res.end(buffer, 'binary');
+    const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': buffer.length,
+    });
+    res.end(buffer, 'binary');
+  } catch (err) {
+    console.error(err);
+    res.status(500).end();
+  }
 }
