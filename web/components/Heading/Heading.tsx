764a1015309064628418054d20bb12a813af4c0f
Jonas Jensen <jonas.jensen@msn.com>
2022-05-02 10:04:10 +0200

    Heading.tsx: specify props for headingsMap prop arg

--- web/components/Heading/Heading.tsx
+++ web/components/Heading/Heading.tsx
@@ -7,12 +7,12 @@ interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
 }
 
 const headingsMap = {
-  h1: (props) => <h1 {...props} />,
-  h2: (props) => <h2 {...props} />,
-  h3: (props) => <h3 {...props} />,
-  h4: (props) => <h4 {...props} />,
-  h5: (props) => <h5 {...props} />,
-  h6: (props) => <h6 {...props} />,
+  h1: (props: HTMLAttributes<HTMLHeadingElement>) => <h1 {...props} />,
+  h2: (props: HTMLAttributes<HTMLHeadingElement>) => <h2 {...props} />,
+  h3: (props: HTMLAttributes<HTMLHeadingElement>) => <h3 {...props} />,
+  h4: (props: HTMLAttributes<HTMLHeadingElement>) => <h4 {...props} />,
+  h5: (props: HTMLAttributes<HTMLHeadingElement>) => <h5 {...props} />,
+  h6: (props: HTMLAttributes<HTMLHeadingElement>) => <h6 {...props} />,
 };
 
 export const Heading = ({ className, type = 'h1', ...props }: HeadingProps) => {
