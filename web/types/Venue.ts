4199004c266318def19ff5b0afbf1d00abe64e3f
Jonas Jensen <jonas.jensen@msn.com>
2022-05-04 12:30:42 +0200

    Venue.ts: update type to correspond with Sanity schema

--- web/types/Venue.ts
+++ web/types/Venue.ts
@@ -1,14 +1,36 @@
-import { Slug } from './Slug';
+import type { Section } from './Section';
+import type { Slug } from './Slug';
 
 export type Venue = {
+  _createdAt: string;
   _id: string;
+  _rev: string;
   _type: 'venue';
-  name: string;
+  _updatedAt: string;
+  accessibility?: {
+    captioning?: boolean;
+    neutralRestroom?: boolean;
+    parking?: boolean;
+    prioritySeating?: boolean;
+    wheelchair?: boolean;
+  };
+  accomodations?: Section[];
+  address?: {
+    city?: string;
+    country?: string;
+    postalCode?: string;
+    state?: string;
+    street?: string;
+    other?: string;
+    zip?: string;
+  };
+  directions?: Section[];
   geolocation?: {
     _type: 'geopoint';
     lat: number;
     lng: number;
   };
+  name: string;
   slug: Slug;
   timezone?: string;
 };
